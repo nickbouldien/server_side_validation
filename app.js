84095d1c8032436655e3eff694a899533b17bed7
learn <learn@learn18.local>
2017-05-23 16:53:00 -0700

    add /users route, add methods in user.js to encrypt password and verify password

--- app.js
+++ app.js
@@ -7,8 +7,36 @@ app.use(express.static('public'))
 app.use(bodyParser.json())
 
 app.get('/', function (request, response) {
-  response.json({message: 'APIasdfasdf App'})
+  response.json({message: 'API Example App'})
 });
+
+app.post('/test', function(request, response){
+  response.json({params: request.body})
+})
+
+app.post('/users', function(request, response){
+  console.log(request.body)
+  User.create(
+    {
+      firstName: request.body.firstName,
+      lastName: request.body.lastName,
+      email: request.body.email,
+      password: request.body.password
+    }
+  ).then((user)=>{
+    response.json({
+      message: 'success',
+      user: user
+    })
+  })
+  .catch((error)=>{
+    response.status(400)
+    response.json({
+      message: "Unable to create User",
+      errors: error.errors
+    })
+  })
+})
 
 app.listen(3000, function () {
  console.log('Todo Server listening on port 3000!');
--- models/user.js
+++ models/user.js
@@ -1,19 +1,127 @@
 'use strict';
+const crypto = require('crypto')
+const uuid = require('uuid/v1')
+
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    encryptedPassword: DataTypes.STRING,
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate:{
+        isEmail: true
+      }
+    },
+    encryptedPassword: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     authToken: DataTypes.STRING,
     authTokenExpiration: DataTypes.DATE,
     salt: DataTypes.STRING
   }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
+    // ... other User setup
+    setterMethods:{
+      // Virtual method for password
+      // Password does not exist in the database, but rather
+      // it is transformed to the 'encryptedPassword' value and stored as that
+      password(value){
+        if(value){
+          const salt = uuid()
+          this.setDataValue('salt', salt)
+          const hash = this.encrypt(value)
+          this.setDataValue('encryptedPassword', hash)
+        }
       }
-    }
+    },
+    instanceMethods:{
+      // Method to encrypt any value using salt from user record
+      encrypt(value){
+        const salt = this.get('salt')
+        return crypto.createHmac('sha512', salt)
+          .update(value)
+          .digest('hex')
+      },
+
+      // Checks to see if passed value matches encrypted password value from record
+      verifyPassword(unverifiedPassword){
+        //encrypt unverifiedPassword
+        const encryptedUnverifiedPassword = this.encrypt(unverifiedPassword)
+
+        //compare encryptedUnverifiedPassword with password
+        return encryptedUnverifiedPassword === this.get('encryptedPassword')
+      },
+      toJSON(){
+        return {
+          id: this.get('id'),
+          firstName: this.get('firstName'),
+          lastName: this.get('lastName'),
+          email: this.get('email'),
+          authToken: this.get('authToken'),
+          authTokenExpiration: this.get('authTokenExpiration')
+        }
+      }
+
+      // ... other instance methods
+    },
   });
   return User;
 };
+
+
+
+
+
+//
+// module.exports = function(sequelize, DataTypes) {
+//   var User = sequelize.define('User', {
+//
+//     //.... Other User fields
+//     encryptedPassword: {
+//       type: DataTypes.STRING,
+//       allowNull: false
+//     },
+//     salt: DataTypes.STRING
+//   }, {
+//     setterMethods:{
+//       // Virtual method for password
+//       // Password does not exist in the database, but rather
+//       // it is transformed to the 'encryptedPassword' value and stored as that
+//       password(value){
+//         if(value){
+//           const salt = uuid()
+//           console.log(this); //
+//           this.setDataValue('salt', salt)
+//           const hash = this.encrypt(value)
+//           this.setDataValue('encryptedPassword', hash)
+//         }
+//       }
+//     },
+//     instanceMethods:{
+//       // Method to encrypt any value using salt from user record
+//       encrypt(value){
+//         const salt = this.get('salt')
+//         return crypto.createHmac('sha512', salt)
+//           .update(value)
+//           .digest('hex')
+//       },
+//       // Checks to see if passed value matches encrypted password value from record
+//       verifyPassword(unverifiedPassword){
+//         //encrypt unverifiedPassword
+//         const encryptedUnverifiedPassword = this.encrypt(unverifiedPassword)
+//
+//         //compare encryptedUnverifiedPassword with password
+//         return encryptedUnverifiedPassword === this.get('encryptedPassword')
+//       },
+//
+//     },
+//   });
+//   return User;
+// };
